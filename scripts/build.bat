323639d756b795d610d6086295ba5e0584a09756
seven <lihong702@crpcg.com>
2024-11-14 09:05:42 +0800

    fix:SpringSecurity升级到5.7.13

--- scripts/build.bat
+++ scripts/build.bat
@@ -18,20 +18,21 @@ rem
 rem apollo config db info
 set apollo_config_db_url="jdbc:mysql://localhost:3306/ApolloConfigDB?characterEncoding=utf8"
 set apollo_config_db_username="root"
-set apollo_config_db_password=""
+set apollo_config_db_password="123456"
 
 rem apollo portal db info
 set apollo_portal_db_url="jdbc:mysql://localhost:3306/ApolloPortalDB?characterEncoding=utf8"
 set apollo_portal_db_username="root"
-set apollo_portal_db_password=""
+set apollo_portal_db_password="123456"
 
 rem meta server url, different environments should have different meta server addresses
-set dev_meta="http://localhost:8080"
-set fat_meta="http://someIp:8080"
-set uat_meta="http://anotherIp:8080"
-set pro_meta="http://yetAnotherIp:8080"
+set dev_meta="http://localhost:6002"
+set fat_meta="http://localhost:6002"
+set uat_meta="http://localhost:6002"
+set lpt_meta="http://localhost:6002"
+set pro_meta="http://localhost:6002"
 
-set META_SERVERS_OPTS=-Ddev_meta=%dev_meta% -Dfat_meta=%fat_meta% -Duat_meta=%uat_meta% -Dpro_meta=%pro_meta%
+set META_SERVERS_OPTS=-Ddev_meta=%dev_meta% -Dfat_meta=%fat_meta% -Duat_meta=%uat_meta% -Dlpt_meta=%lpt_meta% -Dpro_meta=%pro_meta%
 
 rem =============== Please do not modify the following content =============== 
 rem go to script directory
